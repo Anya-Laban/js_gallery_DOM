dcebcd4a6254833477c5a2fd510346f4b8616629
Anya Laban <laban.anya.a@gmail.com>
2024-12-16 14:17:12 +0200

    make build

---
+++ index.14a70f7d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");t.src=r.href});
+//# sourceMappingURL=index.14a70f7d.js.map
